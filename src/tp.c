ba623987b3f495a9281ca90bb181b0cf64db947b
Yusaku Maezono <yusakumaezono@gmail.com>
2021-03-13 16:42:10 +0900

    Add tp_exec.

--- src/tp.c
+++ src/tp.c
@@ -1,6 +1,8 @@
 #include <stdlib.h>
 #include "tp.h"
 
+tp_obj tp_None = {TP_NONE};
+
 tp_vm *tp_init(int argc, char *argv[])
 {
   tp_vm *tp = (tp_vm *)calloc(sizeof(tp_vm), 1);
@@ -15,15 +17,85 @@ tp_obj tp_string_atom(TP, const char *v)
   return r;
 }
 
-tp_obj tp_load(TP, const char *fname)
+tp_obj tp_string_t(TP, int n)
 {
   tp_obj r;
+  return r;
+}
+
+char *tp_string_getptr(tp_obj s)
+{
+  return (char *)s.ptr;
+}
+
+tp_obj tp_load(TP, const char *fname)
+{
+  struct stat stbuf;
+  //stat(fname, &stbuf);
+
+  FILE *f = fopen(fname, "rb");
+  if (!f)
+  {
+    printf("(tp_load) IOError\n");
+    return tp_None;
+  }
+
+  long l = stbuf.st_size;
+
+  tp_obj r = tp_string_t(tp, l);
+
+  char *s = tp_string_getptr(r);
+
+  fread(s, 1, l, f);
+
+  fclose(f);
+
+  return r;
+}
+
+void tpd_list_insertx(TP, tpd_list *self, int n, tp_obj v)
+{
+  /*
+  if (self->len >= self->alloc)
+  {
+    tpd_list_realloc(tp, self, self->alloc * 2);
+  }
+  if (n < self->len)
+  {
+    memmove(&self->items[n + 1], &self->items[n], sizeof(tp_obj) * (self->len - n));
+  }
+  self->items[n] = v;
+  self->len += 1;
+  */
+}
+
+void tpd_list_appendx(TP, tpd_list *self, tp_obj v)
+{
+  tpd_list_insertx(tp, self, self->len, v);
+}
+
+void tp_enter_frame(TP, tp_obj lparams, tp_obj dparams,
+                    tp_obj globals,
+                    tp_obj code,
+                    tp_obj args,
+                    tp_obj defaults,
+                    tp_obj *ret_dest)
+{
+  //tpd_list_appendx(tp, tp->frames, tp_frame_t(tp, lparams, dparams, globals, code, args, defaults, ret_dest));
+}
+
+tp_obj tp_exec(TP, tp_obj code, tp_obj globals)
+{
+  tp_obj r = tp_None;
+  //tp_enter_frame(tp, tp_None, tp_None, globals, code, tp_None, tp_None, &r);
+  //tp_run_frame(tp);
   return r;
 }
 
 tp_obj tp_import(TP, tp_obj name, tp_obj code, tp_obj fname)
 {
   tp_obj g;
+  tp_exec(tp, code, g);
   return g;
 }
 
--- src/tp.h
+++ src/tp.h
@@ -1,13 +1,21 @@
 #ifndef TP_H
 #define TP_H
 
+#include <stdio.h>
+
 #define TP_NO_LIMIT 1
 
 #define TP tp_vm *tp
 
 enum TPTypeID
 {
+  TP_NONE = 0,
   TP_STRING = 100
+};
+
+struct stat
+{
+  off_t st_size;
 };
 
 typedef struct tp_vm
@@ -15,8 +23,21 @@ typedef struct tp_vm
   double time_limit;
 } tp_vm;
 
+typedef struct TPTypeInfo
+{
+  enum TPTypeID typeid;
+} TPTypeInfo;
+
 typedef struct tp_obj
 {
+  TPTypeInfo type;
+  void *info;
+  union
+  {
+    double nfloat;
+    long nint;
+    void *ptr;
+  };
 } tp_obj;
 
 typedef union
@@ -28,6 +49,14 @@ typedef union
   };
   int i;
 } TPGCMask;
+
+typedef struct tpd_list
+{
+  TPGCMask gci;
+  tp_obj *items;
+  int len;
+  int alloc;
+} tpd_list;
 
 typedef struct tpd_string
 {
@@ -43,5 +72,9 @@ tp_obj tp_load(TP, const char *);
 tp_obj tp_import(TP, tp_obj name, tp_obj code, tp_obj fname);
 
 void tp_deinit(TP);
+
+tp_obj tp_string_t(TP, int n);
+
+tp_obj tp_exec(TP, tp_obj code, tp_obj globals);
 
 #endif
